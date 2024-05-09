35fcf60238f5585a10920eb8c652d8ee261d319e
PKDGOOD <sean4787@gmail.com>
2024-05-09 15:28:16 +0900

    add undersampling.py

---
+++ classification/undersampling.py
@@ -0,0 +1,33 @@
+# Random Undersampling
+from collections import Counter
+from sklearn.datasets import make_classification
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.pipeline import Pipeline
+from matplotlib import pyplot
+from numpy import where
+
+# make_classification(): n_redundant (no. of redundant features)
+#                        weights (portion of samples assigned to a class)
+X, y = make_classification(n_samples=10000, n_features=2, n_redundant=0,
+                           n_clusters_per_class=1, weights=[0.99], flip_y=0, random_state=1)
+
+counter = Counter(y)
+print('generated data: %s' % counter)
+for label, _ in counter.items():
+    row_ix = where(y == label)[0]
+    pyplot.scatter(X[row_ix, 0], X[row_ix, 1], label=str(label))
+pyplot.legend()
+pyplot.show()
+
+# sampling_strategy : |minority samples|/|majority samples| after resampling
+under = RandomUnderSampler(sampling_strategy=0.5)
+X, y = under.fit_resample(X, y)
+counter = Counter(y)
+print('Undersampled data: %s' % counter)
+
+for label, _ in counter.items():
+    row_ix = where(y == label)[0]
+    pyplot.scatter(X[row_ix, 0], X[row_ix, 1], label=str(label))
+pyplot.legend()
+pyplot.show()
